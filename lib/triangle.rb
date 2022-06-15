113d3ef4ef552d918af9a1182aa2c48daefd9d07
Heidi Hlavinka <hchlav@gmail.com>
2022-06-15 14:53:45 -0500

    commit tree ac16eeffc5401afaee6c3d47caed97d6751c2a55

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,54 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  
+  attr_accessor :side_1, :side_2, :side_3, :all_sides
+
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+    @all_sides =[@side_1, @side_2, @side_3].sort
+  end
+
+  def kind
+    if valid_triangle
+      begin
+        raise TriangleError
+      end
+    end
+    if side_1 == side_2 && side_2 == side_3
+      :equilateral 
+    elsif side_1 == side_2 || side_1 == side_3 || side_2 == side_3
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+
+  def valid_triangle
+    all_sides.any? do |side|
+      side < 0 || all_sides[0] + all_sides[1] <= all_sides[2]
+    end
+  end
+
+  # |side| side > 0} && side_1 + side_2 > side_3 || side_1 + side_3 > side_2 || side_2 + side_3 > side_1
+  # binding.pry
+
+    # if triangle.class != Triangle
+    # begin
+    #   raise TriangleError
+    # end
+        # raise error if triangle is not valid
+    #valid triangle is side_1 + side_2 > side_3
+    # each side > 0
+    # use @sides from above
+
+  #@sides is an array of numbers
+
+  class TriangleError < StandardError
+  end
+
+
 end
